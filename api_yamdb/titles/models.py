a898d031ef3a86d20bd486a2cf0b9a5563f15834
Georgiy <37848058+SivikGosh@users.noreply.github.com>
2022-10-31 22:14:12 +0300

    created Title model

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
 
+class Category(models.Model):
+    """модель категории"""
+    pass
+
+
 class Title(models.Model):
     """модель произведения"""
     name = models.CharField(
         max_length=100,
         verbose_name='Название', help_text='Название произведения'
     )
-    year = models.
+    year = models.IntegerField(
+        max_length=4, verbose_name='Год', help_text='Год выхода'
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, related_name='titles',
+        verbose_name='Категория', help_text='Категория произведения'
+    )

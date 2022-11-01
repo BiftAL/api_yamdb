d96983ba737fa5042bc7038431f13c81e0547a42
Anatoly <bift@yandex.ru>
2022-11-02 00:16:06 +0300

    change Urls, get reviews, get comments

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,9 +1,10 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 urlpatterns = [
-    path('v1/titles/', include('reviews_comments.urls')),
+    # path('v1/titles/', include('reviews_comments.urls')),
+    re_path(r'^v1/titles/(?P<title_id>[\d]+)/reviews/', include('reviews_comments.urls')),
     path(
         'auth/token/',
         TokenObtainPairView.as_view(),

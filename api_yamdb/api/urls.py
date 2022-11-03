edef7f50326a47ca0536900522a7eff019097eeb
Ирина Московкина <ika11ika@yandex.ru>
2022-11-03 23:01:33 +0300

    Added new user-model field and signup link

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,11 +1,16 @@
 from django.urls import include, path
 from rest_framework_simplejwt.views import TokenObtainPairView
+from .views import UserViewSet
+from rest_framework import routers
 
+router = routers.DefaultRouter()
+router.register(r'signup', UserViewSet, basename='signup')
 
 urlpatterns = [
     path('v1/titles/', include('reviews_comments.urls')),
+    path('v1/auth/', include(router.urls)),
     path(
-        'auth/token/',
+        'v1/auth/token/',
         TokenObtainPairView.as_view(),
         name='token_obtain_pair'
     ),

f6fed900dc27b7531756ebbff8bd1ad113f361a2
Georgiy <37848058+SivikGosh@users.noreply.github.com>
2022-11-13 23:15:56 +0300

    check pep8

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -9,12 +9,6 @@ router_v1.register(r'users', UsersViewSet, basename='users')
 urlpatterns = [
     path('v1/', include('titles.urls')),
     path('v1/auth/', include('users.urls')),
-    # path('v1/users/me/', GetUserInfoView.as_view(), name='me'),
-    # re_path(
-    #     r'^v1/users/(?P<username>[\w.@+-]+)/$',
-    #     UserRUDView.as_view(),
-    #     name='user'
-    # ),
     path('v1/', include(router_v1.urls)),
     re_path(
         r'^v1/titles/(?P<title_id>[\d]+)/reviews/',
